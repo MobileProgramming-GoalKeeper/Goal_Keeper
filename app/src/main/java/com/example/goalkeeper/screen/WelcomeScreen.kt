ea4893a88e0c6fd3eca37ee4b98e7782e3f9bb33
leekeon215 <leekeon215@gmail.com>
2024-05-31 22:22:52 +0900

    add GoalKeeperButton

--- app/src/main/java/com/example/goalkeeper/screen/WelcomeScreen.kt
+++ app/src/main/java/com/example/goalkeeper/screen/WelcomeScreen.kt
@@ -1,10 +1,7 @@
 package com.example.goalkeeper.screen
 
-import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,15 +14,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.goalkeeper.LocalNavGraphViewModelStoreOwner
 import com.example.goalkeeper.R
-import com.example.goalkeeper.Style.loginTextStyle
-import com.example.goalkeeper.component.RectWithRoundedEnds
+import com.example.goalkeeper.component.GoalKeeperButton
 import com.example.goalkeeper.component.GoalKeeperTextField
 import com.example.goalkeeper.viewmodel.GoalKeeperViewModel
 
@@ -57,26 +52,15 @@ fun WelcomeScreen(navController: NavHostController) {
             value = userPassword,
             label = "PASSWORD"
         ) { userPassword = it }
+
         Spacer(modifier = Modifier.padding(20.dp))
-
-        Box(
-            modifier = Modifier.clickable {
-                if(viewModel.login(userID, userPassword))
-                    navController.navigate("main")
-            },
-            contentAlignment = Alignment.Center
-        ) {
-            RectWithRoundedEnds(230, 60, color = colorResource(id = R.color.violet))
-            androidx.compose.material3.Text(text = "로그인/시작하기", style = loginTextStyle)
+        GoalKeeperButton(width = 230, height = 60, text = "로그인/시작하기") {
+            if (viewModel.login(userID, userPassword))
+                navController.navigate("main")
         }
-
         Spacer(modifier = Modifier.padding(10.dp))
-        Box(
-            modifier = Modifier.clickable { navController.navigate("register") },
-            contentAlignment = Alignment.Center
-        ) {
-            RectWithRoundedEnds(230, 60, color = colorResource(id = R.color.violet))
-            androidx.compose.material3.Text(text = "회원가입", style = loginTextStyle)
+        GoalKeeperButton(width = 230, height = 60, text = "회원가입") {
+            navController.navigate("register")
         }
     }
 }
